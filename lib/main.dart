f02b7bc333499dca7b8cd70bca8e9b8437b1b75e
decagon <chiorlujack@gmail.com>
2023-09-14 16:21:18 +0100

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: "/",
+      initialRoute: "/otp",
       routes: {
         '/': (context) => LoginSignupScreen(),
         '/signup': (context) => CreateAccountScreen(),
